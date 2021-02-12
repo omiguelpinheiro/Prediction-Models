3ba8487215802021241473f4ddd44d649b1725c7
Miguel Pinheiro <miguel@astrato.com.br>
2021-02-12 18:03:02 -0300

    feat: add mini batch gradient descent optimizer

--- optimizer/mini_batch_gradient_descent.py
+++ optimizer/mini_batch_gradient_descent.py
@@ -1,11 +1,12 @@
 import numpy as np
+from optimizer.gradient_descent import GradientDescent
 
 """
-Batch Gradient Descent: Is a gradient based optimization method,
-the "Batch" means it uses all of the dataset examples to make one
-update on the model weights. It is the most precise way to reach a
-local / global minima but weights updates takes forever for really
-large datasets.
+Mini Batch Gradient Descent: Is a gradient based optimization method,
+the "Mini Batch" means it uses a mini batch of examples of the dataset
+at a time to make one update on the model weights. It is less precise
+than Batch Gradient Descent but weight updates are faster so it can
+improve convergence time.
 
 Math: Uses Gradient Descent (check gradient_descent.py) math and the
 loss function will be the sum of the mini batch examples errors.
@@ -17,3 +18,8 @@ lr = learning rate (size of step in the -dw vector direction)
 dw = gradient of the loss function with respect to the weights w
 
 """
+
+
+class MiniBatchGradientDescent(GradientDescent):
+	def __init__(self, learning_rate, batch_size):
+		super().__init__(learning_rate, batch_size)
