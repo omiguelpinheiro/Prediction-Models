b2b9d41a5b9b5e534f3ddf388b3242b2ea73e199
Miguel Pinheiro <miguel@astrato.com.br>
2021-02-12 18:04:42 -0300

    feat: add mean squared error loss function

---
+++ loss/mean_squared_error.py
@@ -0,0 +1,33 @@
+"""
+Mean Squared Error (MSE): This loss is useful because it treats big
+errors as more relevant than small errors.
+
+Math: The loss is the mean of the sum of the squared differences 
+between all of the predictions h(x) and their true values y. It's
+common to multiply the MSE by 1 / 2 to make the derivation easier.
+We can do this because what we are trying to do is minimize the
+model error so it can go as low as possible and trying to do this
+to the error and to half of the error is the same thing. We are
+still trying to minimize the error so it can get to it's lowest. 
+
+J(h(x), y) = (1 / m) * sum((h(x) - y) ** 2) * (1 / 2)
+
+J = Mean Squared Error loss function
+h(x) = the model predictions for the x example
+m = number of examples
+sum = sum over all the m examples
+
+"""
+
+
+class MeanSquaredError:
+	def __init__(self):
+		self.dw = None
+		self.loss = None
+
+	def calculate_gradient(self, w, x_train, y_train):
+		m = len(x_train)
+		predictions = x_train.T * w
+		errors = predictions - y_train
+		self.dw = (1 / m) * 2 * errors * x_train
+		self.loss = (1 / 2 * m) * sum(errors ** 2)
