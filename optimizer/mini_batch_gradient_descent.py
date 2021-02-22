bc0a5e2cc87a7a432ed3d90bb85b85b653ff55f5
Miguel Pinheiro <miguel@astrato.com.br>
2021-02-22 14:36:33 -0300

    refactor: change method parameters

--- optimizer/mini_batch_gradient_descent.py
+++ optimizer/mini_batch_gradient_descent.py
@@ -21,5 +21,5 @@ dw = gradient of the loss function with respect to the weights w
 
 
 class MiniBatchGradientDescent(GradientDescent):
-	def __init__(self, learning_rate, batch_size):
-		super().__init__(learning_rate, batch_size)
+	def __init__(self, x_train, y_train, learning_rate, batch_size, random_state=None):
+		super().__init__(x_train, y_train, learning_rate, batch_size, random_state)
