ccdb6c3b7350d1d30dd15496390949971b843fc3
Miguel Pinheiro <miguel@astrato.com.br>
2021-02-22 14:30:51 -0300

    feat: add r2 metric

---
+++ metric/r2_score.py
@@ -0,0 +1,6 @@
+class R2Score:
+	def score(self, y_pred, y_true):
+		numerator = ((y_true - y_pred) ** 2).sum()
+		denominator = ((y_true - y_true.mean()) ** 2).sum()
+		r2_score = 1 - numerator / denominator
+		return r2_score
